15a26a49806aa9155864402278e2180d832fa49f
AfifArdhiansyah <afifardhiansyah@gmail.com>
2022-05-29 21:21:54 +0700

    perbaikan

--- B_5025201212.R
+++ B_5025201212.R
@@ -16,15 +16,18 @@ standev <- sd(diff)
 # b)
 miud <- sum(diff - rat)
 
-t <- (rat - miud) / (standev/sqrt(n))
-
-# c)
 t.test(y, x, paired=TRUE)
 
-#nomor 2
+# c)
 
-zsum.test(mean.x = 23500, sigma.x = 3900, n.x = 100, alternative = "greater", mu = 0,
-          conf.level = 0.95)
+#Karena p-value di bawah nilai signifikan (0.005), maka h0:""tidak ada pengaruh
+# yang signifikan secara statistika dalam hal kadar saturasi oksigen , sebelum 
+# dan sesudah melakukan aktivitas", sehingga kesimpulannya adalah tidak ada
+# pengaruh yang signifikan secara statistika dalam hal kadar saturasi oksigen
+
+#nomor 2
+zsum.test(mean.x = 23500, sigma.x = 3900, n.x = 100, alternative = "greater",
+          mu = 0,conf.level = 0.95)
 
 # a)
 # setuju, karena p-value kurang dari 0.05, sehingga h0 (rata-rata <= 20000) ditolak
@@ -37,22 +40,22 @@ zsum.test(mean.x = 23500, sigma.x = 3900, n.x = 100, alternative = "greater", mu
 
 # nomor 3
 
+
+# nomor 3
+
 #nomor 4
 # a)
 my_data <- read.delim(file.choose())
 
 my_data$Group <- as.factor(my_data$Group)
-my_data$Group = factor(my_data$Group, labels = c("grup1", "grup1", "grup3"))
-
+my_data$Group = factor(my_data$Group, labels = c("grup1", "grup2", "grup3"))
 
 grup1 <- subset(my_data, Group == "grup1")
-grup2 <- subset(my_data, Group == "grup1")
+grup2 <- subset(my_data, Group == "grup2")
 grup3 <- subset(my_data, Group == "grup3")
 
 qqnorm(grup1$Length)
-
 qqnorm(grup2$Length)
-
 qqnorm(grup3$Length)
 
 # berdasarkan plot kuantil normal di atas, tidak ditemukan outlier utama pada homogenitas varians
